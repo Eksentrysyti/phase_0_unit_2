bb1bd145bf015bca870c3b356491ee89caf1bb5f
Steven Leu <steven.c.leu@gmail.com>
2014-05-22 15:20:51 -0700

    added releases 1 & 2

--- week_5/1_die/my_solution.rb
+++ week_5/1_die/my_solution.rb
@@ -1,24 +1,27 @@
 # U2.W5: Die Class 1: Numeric
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 2. Pseudocode
 
-# Input:
-# Output:
-# Steps:
+# Input: numbers of sides for the die
+# Output: returns the number of sides for the die and a random number within the range of sides
+# Steps: 
+# 2.1 Create Class Die that initializes with the sides parameter given
+# 2.2 Define sides method of Die that returns the sides paramenter given
+# 2.3 Define roll method of Die that returns a random number between 1 and <number of sides>
 
 
 # 3. Initial Solution
 
 class Die
   def initialize(sides)
-    # code goes here
+    @sides = sides
   end
   
   def sides
-    # code goes here
+    return @sides
   end
   
   def roll
@@ -37,7 +40,9 @@ end
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
 
-
+puts Die.instance_method(:initialize).arity == 1
+puts die.sides + "<-Should be same as the number of sides initialized"
+puts die.roll + "number must be less than or equal to numbers of sides initialized"
 
 
 
