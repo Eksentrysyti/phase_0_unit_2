4ff0ba414131dfc0f2b9ad24dd2340e68ed097b6
Steven Leu <steven.c.leu@gmail.com>
2014-05-22 09:26:24 -0700

    added release 3

--- week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
+++ week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
@@ -39,6 +39,19 @@ sentence_maker(["hello", "world", "this", "is", "a", "test"])
 
 # 3. Refactored Solution
 
+#The initial solution was concise and I don't see any lines I would like to refactor.  The total method uses a single iterator to add each element to a sum variable, which is then returned (printed to screen for testing).  The sentence_maker method first capitalizes the first element in the array, then using another iterator, prints each element to screen.  It then finally adds the period at the end.
+
+def total(array)
+	sum = 0
+	array.each { |x| sum += x}
+	puts sum
+end
+
+def sentence_maker(array)
+	array[0].capitalize!
+	array.each { |x| print "#{x} "}
+	print "."
+end
 
 
 # 4. Reflection 
