f2145a5225db414cbb811f05d12de981427aa42d
Steven Leu <steven.c.leu@gmail.com>
2014-05-22 16:15:13 -0700

    added solution and reflection

--- week_5/1_die/my_solution.rb
+++ week_5/1_die/my_solution.rb
@@ -18,6 +18,9 @@
 class Die
   def initialize(sides)
     @sides = sides
+    if @sides <= 1
+  		raise ArgumentError.new("Number of sides must be greater than 1")
+  	end
   end
   
   def sides
@@ -25,26 +28,43 @@ class Die
   end
   
   def roll
-    # code goes here
+    return rand(1..@sides)
   end
 end
 
+die = Die.new(10)
+puts die.sides
+puts die.roll
 
 
 # 4. Refactored Solution
 
-
-
-
+#The initial solution does not need to be refactored, as all the methods are simple and readable in their purpose.
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
 
 puts Die.instance_method(:initialize).arity == 1
-puts die.sides + "<-Should be same as the number of sides initialized"
-puts die.roll + "number must be less than or equal to numbers of sides initialized"
-
+puts Die.instance_method(:roll).arity == 0
 
 
 
 # 5. Reflection 
+
+=begin
+Starting this challenge simply required reading the instructions and looking up any new concepts/tools that were required for the challenge (i.e. learning about classes and the rand method for ruby's RNG).  My biggest challenge was actually figuring out how to write driver test code for this challenge.
+
+I needed to search up how to generate random numbers, and doing a Google search gave me some surprisingly incoherent or complicated answers for generating numbers in a range.  I finally found a simple way to generate random numbers after a couple links.
+
+I'm still having a bit of trouble writing Driver Test Code, figuring out to access variables in an initialized object.  I will revisit this later.
+
+This challenge showed me how to write simple classes and gave me practice on what limited knowledge I had about classes from before.
+
+I'm fairly comfortable with the learning competencies, although I didn't implement complex if/else statements, string methods, while/until loops for this challenge.
+
+I liked creating a simple class and methods to go with it, it didn't take long to figure out how to do this challenge.
+
+I found creating the driver test code to be tedious as it wasn't clear on what I was supposed to be testing for.  The instructions simply to "pick 3", so I needed to open up the tests file, translate the test code into plain english, and then rewrite it... I found it redundant.	
+
+	
+=end
