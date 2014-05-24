5554602570517b784367e455d24dd42f4c8d11f1
Steven Leu <steven.c.leu@gmail.com>
2014-05-23 17:12:51 -0700

    release 1

--- week_5/6_validate_credit_card/my_solution.rb
+++ week_5/6_validate_credit_card/my_solution.rb
@@ -1,13 +1,14 @@
 # U2.W5: Class Warfare, Validate a Credit Card Number
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 2. Pseudocode
 
-# Input:
-# Output:
+# Input:  16 digit credit card number
+# Output: true if valid card number, false if invalid card number
 # Steps:
+# => 
 
 
 # 3. Initial Solution
@@ -28,6 +29,22 @@ end
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
+
+# Create card for testing
+good_card = CreditCard.new(1234567890123456)
+
+# Test 1: Checks that one parameter was passed to new CreditCard object test_card
+p good_card.method(:initialize).arity == 1 ? "one parameter passed" : "wrong number of parameters passed"
+
+# Test 2: Checks that the card number has 16 digits
+begin
+	bad_card = CreditCard.new(123456)
+rescue ArgumentError
+	p "Card number is wrong length"
+end
+
+# Test 3: Checks that a bad card returns false
+CreditCard.new(4408041234567892) == false ? "good, bad card caught" : "bad card wasn't caught!"
 
 
 
