8f69a94dfd28fd6345abb097919b185a5ac956d1
Steven Leu <steven.c.leu@gmail.com>
2014-06-18 14:34:54 -0700

    added week 4 challenge 2 and 5

--- week_4/2_creative/make_a_homework_cheater/my_solution.rb
+++ week_4/2_creative/make_a_homework_cheater/my_solution.rb
@@ -5,32 +5,46 @@
 
 # 2. Pseudocode
 
-# Input:
-# Output:
+# Input: Title, Person/place/topic, date, thesis statement
+# Output: Template with above inputs
 # Steps:
+# => Create method essay_writer that takes in all inputs
+# => Output inputs within template
 
 
 # 3. Initial Solution
 
-
+=begin
+def essay_writer(title, topic, date, thesis_statement)
+	puts "#{title}"
+	puts ""
+	puts "#{topic} played an important role in history in the year #{date}.  #{thesis_statement}  #{topic} would be remembered and studied in years to come."
+end
+=end
 
 
 # 4. Refactored Solution
 
-
-
-
-
-
-
+def essay_writer(title, topic, date, thesis_statement)
+	puts "#{title}\n\n"
+	puts "#{topic} played an important role in history in the year #{date}.  #{thesis_statement}  #{topic} would be remembered and studied in years to come."
+end
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
 
+essay_writer("America's First President", "George Washington", 1776, "He was the leader of the colonist armies fighting for independence against the British.  He then became the first president of the United States of America.")
 
+puts "\n"
 
+essay_writer("The End of the War to End All Wars", "World War Two", 1945, "This year marked the end of the greatest war in the history of mankind.")
 
+puts "\n"
+
+essay_writer("Attack on America", "The 9/11 bombings", 2001, "These attacks brought down the Twin Towers in New York and damaged the Pentagon.")
 
 
 # 5. Reflection 
+
+# This challenge was simple once you realize you simply needed to plug in values into a template.  The tricky part was creating a template that was general enough to encompass a variety of topics to be used.
 
 
--- week_4/5_pad_array/my_solution.rb
+++ week_4/5_pad_array/my_solution.rb
@@ -4,13 +4,17 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 1. Pseudocode
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
+# What is the input? An array, the min size of the output array, and an optional padding value
+# What is the output? (i.e. What should the code return?) An array that is at least the size of the input array.  Any added values will be the specified padding value or nil if not specified
 # What are the steps needed to solve the problem?
+# => Create #pad method that takes up to 2 parameters
+# => Check the length of the input array and compare to required array size
+# => Take the difference and add padding values to the end of the array to match required array size
+# => Adapt #pad to #pad!
 
 
 # 2. Initial Solution
